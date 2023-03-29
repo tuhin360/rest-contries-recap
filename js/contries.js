865277237a8666baf3fefe11a19b88beadf8d200
tuhin <tuhin153695@gmail.com>
2023-03-29 09:04:20 +0600

    rest-contries-recap

---
+++ js/contries.js
@@ -0,0 +1,25 @@
+const loadCountries = () =>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+}
+
+const displayCountries = countries =>{
+    console.log(countries[0]);
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(allCountries[0]);
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHTML.join(' ');
+}
+
+
+const getCountryHTML = country =>{
+    return `
+        <div class="country">
+            <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}">
+        </div>
+    `
+}
+
+loadCountries();
